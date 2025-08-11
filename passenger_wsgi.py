860070e4ac470364ea8ccd5e3aa6284d7e6fd95d
sani1415 <sanimohammad786@gmail.com>
2025-08-11 08:54:46 +0600

    Refactor passenger_wsgi.py to simplify app import path
    - Updated the import statement in passenger_wsgi.py to directly reference app_server, enhancing clarity and reducing dependency on the backend folder structure.
    - Adjusted comments for better understanding of the code's purpose.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,8 +4,8 @@
 import sys
 import os
 
-# Ensure the backend folder is in Python path
+# Add backend/ to Python path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
 
 # Import the Flask app from app_server.py
-from backend.app_server import app as application
+from app_server import app as application
